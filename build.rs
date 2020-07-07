5bdc5135a4261a9f34861eaf42df9120bf8c67ef
dignifiedquire <me@dignifiedquire.com>
2020-07-08 00:04:30 +0200

    fix building on non x864_64 archs
    (cherry picked from commit 407cc1ad9e572f3ab3683ccb2509d775a300f9ee)

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
+    #[cfg(target_arch = "x86_64")]
     if target_arch == "x86_64" {
         cc::Build::new()
             .flag("-c")
