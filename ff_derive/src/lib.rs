filecoin-project/fff

fix: ensure to compile asm only on x86_64

ff_derive/src/lib.rs
@@ -743,7 +743,7 @@ fn prime_field_impl(
         limbs: usize,
         modulus_raw: &str,
     ) -> proc_macro2::TokenStream {
-        if limbs == 4 && modulus_raw == BLS_381_FR_MODULUS && cfg!(target_arch = "x86_64") {
+        if limbs == 4 && modulus_raw == BLS_381_FR_MODULUS {
             mul_impl_asm4(a, b)
         } else {
             mul_impl_default(a, b, limbs)
@@ -755,14 +755,20 @@ fn prime_field_impl(
         b: proc_macro2::TokenStream,
     ) -> proc_macro2::TokenStream {
         // x86_64 asm for four limbs
-
         let default_impl = mul_impl_default(a.clone(), b.clone(), 4);
 
         let mut gen = proc_macro2::TokenStream::new();
         gen.extend(quote! {
-            if *::fff::CPU_SUPPORTS_ADX_INSTRUCTION {
-                ::fff::mod_mul_4w_assign(&mut (#a.0).0, &(#b.0).0);
-            } else {
+            #[cfg(target_arch = "x86_64")]
+            {
+                if *::fff::CPU_SUPPORTS_ADX_INSTRUCTION {
+                    ::fff::mod_mul_4w_assign(&mut (#a.0).0, &(#b.0).0);
+                } else {
+                    #default_impl
+                }
+            }
+            #[cfg(not(target_arch = "x86_64"))]
+            {
                 #default_impl
             }
         });
@@ -816,8 +822,124 @@ fn prime_field_impl(
         gen
     }
 
+    fn add_assign_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        if limbs == 4 {
+            add_assign_asm_impl(a, b, limbs)
+        } else {
+            add_assign_default_impl(a, b, limbs)
+        }
+    }
+
+    fn add_assign_asm_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        let mut gen = proc_macro2::TokenStream::new();
+        let default_impl = add_assign_default_impl(a.clone(), b.clone(), limbs);
+
+        gen.extend(quote! {
+            #[cfg(target_arch = "x86_64")]
+            {
+                // This cannot exceed the backing capacity.
+                use std::arch::x86_64::*;
+                use std::mem;
+
+                unsafe {
+                    let mut carry = _addcarry_u64(
+                        0,
+                        (#a.0).0[0],
+                        (#b.0).0[0],
+                        &mut (#a.0).0[0]
+                    );
+                    carry = _addcarry_u64(
+                        carry, (#a.0).0[1],
+                        (#b.0).0[1],
+                        &mut (#a.0).0[1]
+                    );
+                    carry = _addcarry_u64(
+                        carry, (#a.0).0[2],
+                        (#b.0).0[2],
+                        &mut (#a.0).0[2]
+                    );
+                    _addcarry_u64(
+                        carry,
+                        (#a.0).0[3],
+                        (#b.0).0[3],
+                        &mut (#a.0).0[3]
+                    );
+
+                    let mut s_sub: [u64; 4] = mem::uninitialized();
+
+                    carry = _subborrow_u64(
+                        0,
+                        (#a.0).0[0],
+                        MODULUS.0[0],
+                        &mut s_sub[0]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[1],
+                        MODULUS.0[1],
+                        &mut s_sub[1]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[2],
+                        MODULUS.0[2],
+                        &mut s_sub[2]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[3],
+                        MODULUS.0[3],
+                        &mut s_sub[3]
+                    );
+
+                    if carry == 0 {
+                        // Direct assign fails since size can be 4 or 6
+                        // Obviously code doesn't work at all for size 6
+                        // (#a).0 = s_sub;
+                        (#a.0).0[0] = s_sub[0];
+                        (#a.0).0[1] = s_sub[1];
+                        (#a.0).0[2] = s_sub[2];
+                        (#a.0).0[3] = s_sub[3];
+                    }
+                }
+            }
+            #[cfg(not(target_arch = "x86_64"))]
+            {
+                #default_impl
+            }
+        });
+
+        gen
+    }
+
+    fn add_assign_default_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        _limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        let mut gen = proc_macro2::TokenStream::new();
+
+        gen.extend(quote! {
+            // This cannot exceed the backing capacity.
+            #a.0.add_nocarry(&#b.0);
+
+            // However, it may need to be reduced.
+            #a.reduce();
+        });
+        gen
+    }
+
     let squaring_impl = sqr_impl(quote! {self}, limbs);
     let multiply_impl = mul_impl(quote! {self}, quote! {other}, limbs, modulus_raw);
+    let add_assign = add_assign_impl(quote! {self}, quote! {other}, limbs);
     let montgomery_impl = mont_impl(limbs);
 
     // (self.0).0[0], (self.0).0[1], ..., 0, 0, 0, 0, ...
@@ -962,79 +1084,7 @@ fn prime_field_impl(
 
             #[inline]
             fn add_assign(&mut self, other: &#name) {
-                if #limbs == 4 && cfg!(target_arch = "x86_64") {
-                    // This cannot exceed the backing capacity.
-                    use std::arch::x86_64::*;
-                    use std::mem;
-
-                    unsafe {
-                        let mut carry = _addcarry_u64(
-                            0,
-                            (self.0).0[0],
-                            (other.0).0[0],
-                            &mut (self.0).0[0]
-                        );
-                        carry = _addcarry_u64(
-                            carry, (self.0).0[1],
-                            (other.0).0[1],
-                            &mut (self.0).0[1]
-                        );
-                        carry = _addcarry_u64(
-                            carry, (self.0).0[2],
-                            (other.0).0[2],
-                            &mut (self.0).0[2]
-                        );
-                        _addcarry_u64(
-                            carry,
-                            (self.0).0[3],
-                            (other.0).0[3],
-                            &mut (self.0).0[3]
-                        );
-
-                        let mut s_sub: [u64; 4] = mem::uninitialized();
-
-                        carry = _subborrow_u64(
-                            0,
-                            (self.0).0[0],
-                            MODULUS.0[0],
-                            &mut s_sub[0]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[1],
-                            MODULUS.0[1],
-                            &mut s_sub[1]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[2],
-                            MODULUS.0[2],
-                            &mut s_sub[2]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[3],
-                            MODULUS.0[3],
-                            &mut s_sub[3]
-                        );
-
-                        if carry == 0 {
-                            // Direct assign fails since size can be 4 or 6
-                            // Obviously code doesn't work at all for size 6
-                            // (self.0).0 = s_sub;
-                            (self.0).0[0] = s_sub[0];
-                            (self.0).0[1] = s_sub[1];
-                            (self.0).0[2] = s_sub[2];
-                            (self.0).0[3] = s_sub[3];
-                        }
-                    }
-                } else {
-                    // This cannot exceed the backing capacity.
-                    self.0.add_nocarry(&other.0);
-
-                    // However, it may need to be reduced.
-                    self.reduce();
-                }
+                #add_assign
             }
 
             #[inline]

zkcrypto/ff

(cherry picked from commit 39cb448c2574bf81e48d915e8f885d5ea062f109)

ff_derive/src/lib.rs
@@ -815,7 +815,7 @@ fn prime_field_impl(
         limbs: usize,
         modulus_raw: &str,
     ) -> proc_macro2::TokenStream {
-        if limbs == 4 && modulus_raw == BLS_381_FR_MODULUS && cfg!(target_arch = "x86_64") {
+        if limbs == 4 && modulus_raw == BLS_381_FR_MODULUS {
             mul_impl_asm4(a, b)
         } else {
             mul_impl_default(a, b, limbs)
@@ -827,14 +827,20 @@ fn prime_field_impl(
         b: proc_macro2::TokenStream,
     ) -> proc_macro2::TokenStream {
         // x86_64 asm for four limbs
-
         let default_impl = mul_impl_default(a.clone(), b.clone(), 4);
 
         let mut gen = proc_macro2::TokenStream::new();
         gen.extend(quote! {
-            if *::ff::CPU_SUPPORTS_ADX_INSTRUCTION {
-                ::ff::mod_mul_4w_assign(&mut (#a.0).0, &(#b.0).0);
-            } else {
+            #[cfg(target_arch = "x86_64")]
+            {
+                if *::ff::CPU_SUPPORTS_ADX_INSTRUCTION {
+                    ::ff::mod_mul_4w_assign(&mut (#a.0).0, &(#b.0).0);
+                } else {
+                    #default_impl
+                }
+            }
+            #[cfg(not(target_arch = "x86_64"))]
+            {
                 #default_impl
             }
         });
@@ -915,9 +921,125 @@ fn prime_field_impl(
         }
     }
 
+    fn add_assign_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        if limbs == 4 {
+            add_assign_asm_impl(a, b, limbs)
+        } else {
+            add_assign_default_impl(a, b, limbs)
+        }
+    }
+
+    fn add_assign_asm_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        let mut gen = proc_macro2::TokenStream::new();
+        let default_impl = add_assign_default_impl(a.clone(), b.clone(), limbs);
+
+        gen.extend(quote! {
+            #[cfg(target_arch = "x86_64")]
+            {
+                // This cannot exceed the backing capacity.
+                use std::arch::x86_64::*;
+                use std::mem;
+
+                unsafe {
+                    let mut carry = _addcarry_u64(
+                        0,
+                        (#a.0).0[0],
+                        (#b.0).0[0],
+                        &mut (#a.0).0[0]
+                    );
+                    carry = _addcarry_u64(
+                        carry, (#a.0).0[1],
+                        (#b.0).0[1],
+                        &mut (#a.0).0[1]
+                    );
+                    carry = _addcarry_u64(
+                        carry, (#a.0).0[2],
+                        (#b.0).0[2],
+                        &mut (#a.0).0[2]
+                    );
+                    _addcarry_u64(
+                        carry,
+                        (#a.0).0[3],
+                        (#b.0).0[3],
+                        &mut (#a.0).0[3]
+                    );
+
+                    let mut s_sub: [u64; 4] = mem::uninitialized();
+
+                    carry = _subborrow_u64(
+                        0,
+                        (#a.0).0[0],
+                        MODULUS.0[0],
+                        &mut s_sub[0]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[1],
+                        MODULUS.0[1],
+                        &mut s_sub[1]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[2],
+                        MODULUS.0[2],
+                        &mut s_sub[2]
+                    );
+                    carry = _subborrow_u64(
+                        carry,
+                        (#a.0).0[3],
+                        MODULUS.0[3],
+                        &mut s_sub[3]
+                    );
+
+                    if carry == 0 {
+                        // Direct assign fails since size can be 4 or 6
+                        // Obviously code doesn't work at all for size 6
+                        // (#a).0 = s_sub;
+                        (#a.0).0[0] = s_sub[0];
+                        (#a.0).0[1] = s_sub[1];
+                        (#a.0).0[2] = s_sub[2];
+                        (#a.0).0[3] = s_sub[3];
+                    }
+                }
+            }
+            #[cfg(not(target_arch = "x86_64"))]
+            {
+                #default_impl
+            }
+        });
+
+        gen
+    }
+
+    fn add_assign_default_impl(
+        a: proc_macro2::TokenStream,
+        b: proc_macro2::TokenStream,
+        _limbs: usize,
+    ) -> proc_macro2::TokenStream {
+        let mut gen = proc_macro2::TokenStream::new();
+
+        gen.extend(quote! {
+            // This cannot exceed the backing capacity.
+            #a.0.add_nocarry(&#b.0);
+
+            // However, it may need to be reduced.
+            #a.reduce();
+        });
+        gen
+    }
+
     let squaring_impl = sqr_impl(quote! {self}, limbs);
     let multiply_impl = mul_impl(quote! {self}, quote! {other}, limbs, modulus_raw);
     let invert_impl = inv_impl(quote! {self}, name, modulus);
+    let add_assign = add_assign_impl(quote! {self}, quote! {other}, limbs);
     let montgomery_impl = mont_impl(limbs);
 
     // self.0[0].ct_eq(&other.0[0]) & self.0[1].ct_eq(&other.0[1]) & ...
@@ -1092,79 +1214,7 @@ fn prime_field_impl(
         impl<'r> ::core::ops::AddAssign<&'r #name> for #name {
             #[inline]
             fn add_assign(&mut self, other: &#name) {
-                if #limbs == 4 && cfg!(target_arch = "x86_64") {
-                    // This cannot exceed the backing capacity.
-                    use std::arch::x86_64::*;
-                    use std::mem;
-
-                    unsafe {
-                        let mut carry = _addcarry_u64(
-                            0,
-                            (self.0).0[0],
-                            (other.0).0[0],
-                            &mut (self.0).0[0]
-                        );
-                        carry = _addcarry_u64(
-                            carry, (self.0).0[1],
-                            (other.0).0[1],
-                            &mut (self.0).0[1]
-                        );
-                        carry = _addcarry_u64(
-                            carry, (self.0).0[2],
-                            (other.0).0[2],
-                            &mut (self.0).0[2]
-                        );
-                        _addcarry_u64(
-                            carry,
-                            (self.0).0[3],
-                            (other.0).0[3],
-                            &mut (self.0).0[3]
-                        );
-
-                        let mut s_sub: [u64; 4] = mem::uninitialized();
-
-                        carry = _subborrow_u64(
-                            0,
-                            (self.0).0[0],
-                            MODULUS.0[0],
-                            &mut s_sub[0]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[1],
-                            MODULUS.0[1],
-                            &mut s_sub[1]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[2],
-                            MODULUS.0[2],
-                            &mut s_sub[2]
-                        );
-                        carry = _subborrow_u64(
-                            carry,
-                            (self.0).0[3],
-                            MODULUS.0[3],
-                            &mut s_sub[3]
-                        );
-
-                        if carry == 0 {
-                            // Direct assign fails since size can be 4 or 6
-                            // Obviously code doesn't work at all for size 6
-                            // (self.0).0 = s_sub;
-                            (self.0).0[0] = s_sub[0];
-                            (self.0).0[1] = s_sub[1];
-                            (self.0).0[2] = s_sub[2];
-                            (self.0).0[3] = s_sub[3];
-                        }
-                    }
-                } else {
-                    // This cannot exceed the backing capacity.
-                    self.0.add_nocarry(&other.0);
-
-                    // However, it may need to be reduced.
-                    self.reduce();
-                }
+                #add_assign
             }
         }
 