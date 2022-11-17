31fec8b9e846e4464467060058db96fbd4e588e0
Evan Laufer <evan.m.laufer@gmail.com>
2022-11-16 17:22:34 -0800

    Bump bitvec to 1.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
 /// Bit representation of a field element.
 #[cfg(feature = "bits")]
 #[cfg_attr(docsrs, doc(cfg(feature = "bits")))]
-pub type FieldBits<V> = BitArray<Lsb0, V>;
+pub type FieldBits<V> = BitArray<V, Lsb0>;
 
 /// This trait represents an element of a field.
 pub trait Field:
