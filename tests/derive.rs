1c2a6c2781096a9bea2a2d8bff0ef733723e9d3a
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2022-12-29 09:36:20 -0800

    Move tests as per Code Review

--- tests/derive.rs
+++ tests/derive.rs
@@ -155,3 +155,21 @@ fn sqrt() {
     test(Fp::ONE);
     test(Fp::random(&mut rand::rng()));
 }
+
+#[cfg(feature = "zero")]
+
+#[test]
+fn zeroize() {
+    use ff::{Field, PrimeField};
+    use rand::rngs::OsRng;
+    use zeroize::Zeroize;
+
+    let mut f = Bls381K12Scalar::random(OsRng);
+    let mut r = f.to_repr();
+
+    f.zeroize();
+    assert_eq!(f, Bls381K12Scalar::ZERO);
+
+    r.zeroize();
+    assert_eq!(r, Default::default());
+}
